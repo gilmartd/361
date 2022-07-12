926423e3d2070b86fef4a7b6497cfe4586d80e11
gilmartd <91555362+gilmartd@users.noreply.github.com>
2022-07-11 21:58:54 -0700

    Add files via upload

--- website/views.py
+++ website/views.py
@@ -13,31 +13,34 @@ def home():
 
     Stats = Pile.query.filter_by(user_id=current_user.id).first()
     if Stats:
-        displayRatio = Stats.ratio
+        displayRatio = round(Stats.ratio,2)
+        displayRatio = str(displayRatio)+ ":1 Carbon:Nitrogen"
     else: displayRatio = "0 because you need to Create a Pile!"
 
 
     if request.method == 'POST':
-        ingredient = int(request.form.get('ingredient'))
-        volumeAdd = request.form.get('volume')
-        if len(volumeAdd) <= 0:
-            flash('Volume cannot be zero', category='error')
-        else:
-            eventCreate = PileUpdate(ratio=ingredient, volume=volumeAdd, user_id=current_user.id)
-            db.session.add(eventCreate)
-            db.session.commit()
-            currentPile = Pile.query.filter_by(user_id= current_user.id).first()
-            #print(currentPile)
-            volume = currentPile.volume
-            volumeUpdate = volume + int(volumeAdd)
-            carbonAdd = int(ingredient)*int(volumeAdd)
-            ratio = currentPile.ratio
-            carbonTotal = ratio*volume
-            carbonTotal += carbonAdd
-            ratioNew = carbonTotal/volumeUpdate
-            currentPile.volume = volumeUpdate
-            currentPile.ratio = ratioNew
-            db.session.commit()
-            flash('You added to your pile!', category='success')
-            return redirect(url_for('views.home'))
+        if Stats:
+            ingredient = int(request.form.get('ingredient'))
+            volumeAdd = request.form.get('volume')
+            if len(volumeAdd) <= 0:
+                flash('Volume cannot be zero', category='error')
+            else:
+                eventCreate = PileUpdate(ratio=ingredient, volume=volumeAdd, user_id=current_user.id)
+                db.session.add(eventCreate)
+                db.session.commit()
+                currentPile = Pile.query.filter_by(user_id= current_user.id).first()
+                #print(currentPile)
+                volume = currentPile.volume
+                volumeUpdate = volume + int(volumeAdd)
+                carbonAdd = int(ingredient)*int(volumeAdd)
+                ratio = currentPile.ratio
+                carbonTotal = ratio*volume
+                carbonTotal += carbonAdd
+                ratioNew = carbonTotal/volumeUpdate
+                currentPile.volume = volumeUpdate
+                currentPile.ratio = ratioNew
+                db.session.commit()
+                flash('You added to your pile!', category='success')
+                return redirect(url_for('views.home'))
+        else: flash('You do not have a pile yet, please create a pile', category='error')
     return render_template("home.html", user=current_user, ratio= displayRatio)
